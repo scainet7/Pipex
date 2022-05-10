a47fbc4630145238fee56538e162497e160415f3
snino <scainet7@mail.ru>
2022-05-10 18:36:44 +0300

    v0.6

--- snino_pipex/pipex.c
+++ snino_pipex/pipex.c
@@ -2,15 +2,8 @@
 
 int main(void)
 {
-<<<<<<< HEAD
-
-	printf("Hello, World!\n");
-	return 0;
-	
-=======
 	int a = 5;
 	a++;
 	printf("Hello, World!\n");
 	return 0;
->>>>>>> 77a87ba782dc28ace99db35fa0b1451617af291a
 }
--- snino_pipex/pipex.h
+++ snino_pipex/pipex.h
@@ -1,10 +1,6 @@
 #ifndef PIPEX_H
 #define PIPEX_H
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 77a87ba782dc28ace99db35fa0b1451617af291a
 #include <stdio.h>
 
 #endif //PIPEX_H
