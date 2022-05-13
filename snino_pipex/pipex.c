7c42ab9dad7bc7904ee2b1d8acc2dfd83246fbc5
scainet7 <scainet7@mail.ru>
2022-05-13 17:59:12 +0300

    v0.7

--- snino_pipex/pipex.c
+++
@@ -1,9 +0,0 @@
-#include "pipex.h"
-
-int main(void)
-{
-	int a = 5;
-	a++;
-	printf("Hello, World!\n");
-	return 0;
-}
--- snino_pipex/pipex.h
+++
@@ -1,6 +0,0 @@
-#ifndef PIPEX_H
-#define PIPEX_H
-
-#include <stdio.h>
-
-#endif //PIPEX_H
