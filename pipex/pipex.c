28405688653bfbf04c1f0a841ecfecfadb8f620a
snino <scainet7@mail.ru>
2022-05-14 19:15:21 +0300

    v0.10

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -1,5 +1,16 @@
 #include "pipex.h"
 
+void fd_error(char *s)
+{
+	printf("%s",s);
+	exit(EXIT_FAILURE);
+}
+/*
+char fd_error(char *s)
+{
+
+}
+
 int fd_parent()
 {
 
@@ -9,15 +20,22 @@ int fd_child()
 {
 
 }
-
+*/
 int main(int argc, char **argv, char **envp)
 {
-    int proc;
-
     if (argc != 5)
-        exit(EXIT_FAILURE);
+    {
+    	if (argc < 5)
+    		fd_error("\033[31m ERROR_MORE_ARGUMENTS \0m\n");
+    	else
+    		fd_error("\033[31m ERROR_MORE_ARGUMENTS \0m\n");
+    }
+    if (envp != 0)
+    	exit(EXIT_FAILURE);
+    /*int proc;
+    pid_t file[2];
     if (proc == 0)
-        fd_child();
-    fd_parent();
+        fd_child(argc, *argv, envp, file[2]);
+    fd_parent();*/
     return 0;
 }
