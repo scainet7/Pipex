b80c38834054b86c5e0cd7e849cf7791e0f1618a
snino <scainet7@mail.ru>
2022-05-15 18:14:12 +0300

    v0.13

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -1,44 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: snino <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/15 18:09:28 by snino             #+#    #+#             */
+/*   Updated: 2022/05/15 18:09:33 by snino            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "pipex.h"
 
-void fd_error(char *s)
+void ft_error(char *s)
 {
 	ft_putstr_fd(s,2);
 	exit(EXIT_FAILURE);
 }
-
-/*void fd_parent(int argc, char **argv, char **envp, int *pipe_fd)
+void ft_first_cmd(char *argv, char **envp)
 {
 
 }
-
-void fd_descendant(int argc, char **argv, char **envp, int *pipe_fd)
+/*void fd_parent(int argc, char **argv, char **envp, t_parametrs params)
 {
 
 }
 */
+void fd_descendant(char **argv, char **envp, t_parametrs params)
+{
+	if (access(argv[1], F_OK))
+	{
+		params.pid_first = open(argv[1], O_RDONLY | O_CREAT, 0644);
+	}
+	else if (access(argv[1], R_OK) != 0)
+		ft_error(RED"ERROR_INFILE_READ"END);
+	else if (params.pid_first < 0)
+		ft_error(RED"ERROR_OPEN_INFILE"END);
+	else if (dup2(params.pipe_fd[0], 0) < 0 || dup2(params.pid_first, 1) < 0
+	   || close(params.pid_first) < 0 || close(params.pipe_fd[0]) < 0
+	   || close(params.pipe_fd[1]) < 0)
+		ft_error(RED"ERROR_DUP2"END);
+	printf(GREEN"%s"END, envp[1]);
+	ft_first_cmd(argv[3], envp);
+}
+
 int main(int argc, char **argv, char **envp)
 {
-	//t_parametrs params;
+	t_parametrs params;
     if (argc != 5)
     {
     	if (argc < 5)
-    		fd_error(RED" ERROR_MANY_ARGUMENTS"END);
+    		ft_error(RED" ERROR_MANY_ARGUMENTS"END);
     	else
-    		fd_error(RED"ERROR_MORE_ARGUMENTS"END);
+    		ft_error(RED"ERROR_MORE_ARGUMENTS"END);
     }
     if (envp == 0)
-		fd_error(RED"ERROR_ENVP"END);
-	int pid;
-	pid = fork();
-	if (pid < 0)
-		fd_error(RED"ERROR_FORK"END);
-	if (pid == 0)
-	{
-		printf("%d\n", argc);
-		printf("%s\n", argv[0]);
-	}
-		//fd_descendant(argc, argv, envp, params.pipe_fd);
-	//fd_parent(argc, argv, envp, params.pipe_fd);
+		ft_error(RED"ERROR_ENVP"END);
+	params.pid_first = fork();
+	if (params.pid_first < 0)
+		ft_error(RED"ERROR_FORK"END);
+	if (params.pid_first == 0)
+		fd_descendant(argv, envp, params);
+	//fd_parent(argc, argv, envp, params);
 
     return 0;
 }
--- pipex/pipex.h
+++ pipex/pipex.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: snino <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/15 18:09:42 by snino             #+#    #+#             */
+/*   Updated: 2022/05/15 18:09:47 by snino            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PIPEX_H
 #define PIPEX_H
 
@@ -8,6 +20,8 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include "libft/libft.h"
 
@@ -18,6 +32,11 @@ typedef struct s_parametrs
 	pid_t pid_first;
 	pid_t pid_second;
 	int pipe_fd[2];
+	int infile;
+	int outfile;
 } t_parametrs;
 
+void fd_descendant(char **argv, char **envp, t_parametrs params);
+void ft_error(char *s);
+void ft_first_cmd(char *argv, char **envp);
 #endif //PIPEX_H
