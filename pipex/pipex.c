1cabd73c8bf8387b416300c9203114bfc2d15ed5
scainet7 <scainet7@mail.ru>
2022-05-18 15:18:55 +0300

    v0.14

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -17,6 +17,21 @@ void ft_error(char *s)
 	ft_putstr_fd(s,2);
 	exit(EXIT_FAILURE);
 }
+
+void check_fd(char **argv, t_parametrs params)
+{
+    if (access(argv[1], F_OK))
+        ft_error(RED"ERROR_INFILE_DOES_NOT_EXIST"END);
+    else if (access(argv[1], R_OK) != 0)
+        ft_error(RED"ERROR_INFILE_READ"END);
+    params.infile = open(argv[1], O_RDONLY);
+    if (access(argv[4], F_OK))
+        params.outfile = open(argv[4], O_RDWR | O_CREAT | O_TRUNC, 0644);
+    else if (access(argv[4], W_OK)
+        ft_error(RED"ERROR_OUTFILE_READ"END);
+    params.outfile = open(argv[4], O_RDWR | O_TRUNC);
+}
+
 void ft_first_cmd(char *argv, char **envp)
 {
 
@@ -28,14 +43,11 @@ void ft_first_cmd(char *argv, char **envp)
 */
 void fd_descendant(char **argv, char **envp, t_parametrs params)
 {
-	if (access(argv[1], F_OK))
-	{
-		params.pid_first = open(argv[1], O_RDONLY | O_CREAT, 0644);
-	}
-	else if (access(argv[1], R_OK) != 0)
-		ft_error(RED"ERROR_INFILE_READ"END);
-	else if (params.pid_first < 0)
-		ft_error(RED"ERROR_OPEN_INFILE"END);
+    dup2(params.infile, 0);
+    dup2(params.pipe_fd[1], 1);
+    params.cmd_paths = ft_split(argv[2], ' ');
+    params.paths = find_path(envp);
+
 	else if (dup2(params.pipe_fd[0], 0) < 0 || dup2(params.pid_first, 1) < 0
 	   || close(params.pid_first) < 0 || close(params.pipe_fd[0]) < 0
 	   || close(params.pipe_fd[1]) < 0)
@@ -56,6 +68,9 @@ int main(int argc, char **argv, char **envp)
     }
     if (envp == 0)
 		ft_error(RED"ERROR_ENVP"END);
+    check_fd(argv, params);
+    if (pipe(params.pipe_fd) < 0)
+        ft_error(RED"ERROR_PIPE"END);
 	params.pid_first = fork();
 	if (params.pid_first < 0)
 		ft_error(RED"ERROR_FORK"END);
